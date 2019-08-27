dc447f70efe5d70aab19f75d0a5309a5fa50f226
Matt M <matt.d.m@hotmail.com>
2019-08-27 12:33:32 -0500

    added connect

--- bucket-list/src/components/AddItemModal.jsx
+++ bucket-list/src/components/AddItemModal.jsx
@@ -3,6 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import Modal from "@material-ui/core/Modal";
 import Backdrop from "@material-ui/core/Backdrop";
 import { useSpring, animated } from "react-spring";
+import { connect } from "react-redux";
 import { Form, Field, withFormik } from "formik";
 import AddIcon from "@material-ui/icons/Add";
 import IconButton from "@material-ui/core/IconButton";
