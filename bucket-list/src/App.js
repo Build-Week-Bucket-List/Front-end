1e38b9b113e3935d7baa23dbb6e97a8f5e6803fb
Eli Sacks <bastlifa@gmail.com>
2019-08-26 21:57:45 -0400

    Fixed RegisterWithFormik, fixed login token assign

--- bucket-list/src/App.js
+++ bucket-list/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Link } from 'react-router-dom'
 import Login from './components/OnBoarding/Login'
 import Register from './components/OnBoarding/Register'
+import RegisterWithFormik from './components/OnBoarding/RegisterWithFormik'
 import BucketList from './components/Bucket/BucketList'
 import PrivateRoute from './components/OnBoarding/PrivateRoute'
 import './App.css';
@@ -10,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <Route exact path="/" component={Login} />
-      <Route path="/register" component={Register} />
+      <Route path="/register" component={RegisterWithFormik} />
       <PrivateRoute path="/home" component={BucketList} />
     </div>
   );
